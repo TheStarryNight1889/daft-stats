88a77054d6d335893b40fbea945489c49e4e4d45
TheStarryNight1889 <molloy.christie@gmail.com>
2022-06-06 22:34:22 +0100

    inital scraping

---
+++ main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"context"
+	"daft-stats/models"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/gocolly/colly"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// Connection URI
+const uri = "mongodb://localhost:27017"
+
+func main() {
+	property := models.Property{Price: 1200, Location: "Dublin", URL: "https://bullshit.com/adfsjkl/12345", ID: 1234567}
+	// Create a new client and connect to the server
+	credential := options.Credential{
+		Username: "root",
+		Password: "example",
+	}
+	clientOpts := options.Client().ApplyURI(uri).SetAuth(credential)
+	client, err := mongo.Connect(context.TODO(), clientOpts)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully connected and pinged.")
+	coll := client.Database("daft-stats").Collection("properties")
+	_, err = coll.InsertOne(context.TODO(), property)
+
+	property_links := scrape_properties()
+
+	fmt.Print(build_properties_from_links(property_links))
+
+}
+func build_properties_from_links(links []string) []models.Property {
+	properties := []models.Property{}
+	var price int
+	var url string
+	var location string
+	var id int
+	var beds int
+	var baths int
+	var type_ string
+	var entered string
+	var views int
+
+	c := colly.NewCollector()
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	c.OnHTML("div[data-testid=price]", func(h *colly.HTMLElement) {
+		if strings.Contains(h.Text, "€") {
+			string_price := strings.Split(h.Text, " ")[0]
+			removed_euro := strings.Split(string_price, "€")[1]
+			clean_price := strings.Replace(removed_euro, ",", "", -1)
+			price_results, err := strconv.Atoi(clean_price)
+			if err != nil {
+				fmt.Errorf("Error converting to int%s", err)
+			}
+			price = price_results
+		}
+	})
+
+	c.OnHTML("h1[data-testid=address]", func(h *colly.HTMLElement) {
+		location = h.Text
+	})
+
+	c.OnHTML("p [data-testid=beds]", func(h *colly.HTMLElement) {
+		beds_temp, err := strconv.Atoi(strings.Split(h.Text, " ")[0])
+		if err != nil {
+			fmt.Errorf("Error converting to in%s", err)
+		}
+		beds = beds_temp
+	})
+
+	c.OnHTML("p [data-testid=baths]", func(h *colly.HTMLElement) {
+		baths_temp, err := strconv.Atoi(strings.Split(h.Text, " ")[0])
+		if err != nil {
+			fmt.Errorf("Error converting to in%s", err)
+		}
+		baths = baths_temp
+	})
+
+	c.OnHTML("p [data-testid=property-type]", func(h *colly.HTMLElement) {
+		type_ = strings.Split(h.Text, " ")[0]
+	})
+	c.OnHTML("div [data-testid=statistics]", func(h *colly.HTMLElement) {
+		stats := h.ChildText("p")
+		stats_split := strings.Split(stats, " ")
+		views_temp, err := strconv.Atoi(strings.Replace(stats_split[2], ",", "", -1))
+		if err != nil {
+			fmt.Errorf("Error converting to int%s", err)
+		}
+		views = views_temp
+		entered = stats_split[0]
+	})
+
+	for _, l := range links {
+
+		url_to_visit := fmt.Sprintf("https://www.daft.ie%s", l)
+		c.Visit(url_to_visit)
+
+		url = url_to_visit
+		split_url := strings.Split(url_to_visit, "/")
+		temp_id, err := strconv.Atoi(split_url[len(split_url)-1])
+		if err != nil {
+			fmt.Errorf("Error converting to int%s", err)
+		}
+		id = temp_id
+
+		property := models.Property{}
+		property.Price = price
+		property.URL = url
+		property.Location = location
+		property.ID = id
+		property.Bed = beds
+		property.Bathroom = baths
+		property.Type = type_
+		property.Entered = entered
+		property.Views = views
+
+		properties = append(properties, property)
+	}
+	return properties
+}
+
+func scrape_properties() []string {
+	from_value := 0
+	c := colly.NewCollector()
+	// On every a element which has href attribute call callback
+	links := []string{}
+
+	c.OnHTML("li a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+
+		if strings.Contains(link, "/for-rent/") {
+			links = append(links, link)
+		}
+
+	})
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	// Start scraping on https://hackerspaces.org
+	for i := from_value; i < 40; i += 20 {
+		page_to_visit := fmt.Sprintf("https://www.daft.ie/property-for-rent/ireland?pageSize=20&from=%d", i)
+		c.Visit(page_to_visit)
+	}
+	return links
+}
---
+++ models/property.go
@@ -0,0 +1,14 @@
+package models
+
+type Property struct {
+	Price    int    `bson: price`
+	Location string `bson: location`
+	URL      string `bson: url`
+	ID       int    `bson: id`
+	Entered  string `bson: entered`
+	Views    int    `bson: views`
+	Type     string `bson: type`
+	Bathroom int    `bson: bathroom`
+	Bed      int    `bson: bed`
+}
+
