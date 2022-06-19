b65bdc214d208a4e70aa4588f566363493c565f5
christie_molloy <christie.molloy@viotas.com>
2022-06-20 00:42:26 +0100

    added store

---
+++ client/src/stores/propertyStore.js
@@ -0,0 +1,13 @@
+import { defineStore } from 'pinia';
+
+const useProperties = defineStore('properties', {
+  state: () => ({
+    properties: [],
+  }),
+  getters: {
+  },
+  actions: {
+  },
+});
+
+export default useProperties;
