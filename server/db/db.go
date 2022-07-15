eced0fee55a94abbc9d5c2cacda01e7531bc5f37
TheStarryNight1889 <molloy.christie@gmail.com>
2022-07-15 22:40:09 +0100

    updated how db con is handled

--- server/db/db.go
+++ server/db/db.go
@@ -11,34 +11,8 @@ import (
 
 func Connect() *mongo.Database {
 
-	//  if env DB_PORT, DB_HOST are set, use them
-	//  otherwise use default values
-	port := os.Getenv("DB_PORT")
-	if port == "" {
-		port = "27017"
-	}
-	host := os.Getenv("DB_HOST")
-	if host == "" {
-		host = "mongodb://localhost"
-	}
-	// build connection URI
-	uri := fmt.Sprintf("%s:%s", host, port)
-
-	// if env DB_USER, DB_PASS are set, use them
-	// otherwise use default values
-	user := os.Getenv("DB_USER")
-	if user == "" {
-		user = "root"
-	}
-	pass := os.Getenv("DB_PASS")
-	if pass == "" {
-		pass = "example"
-	}
-	credential := options.Credential{
-		Username: user,
-		Password: pass,
-	}
-	clientOpts := options.Client().ApplyURI(uri).SetAuth(credential)
+	mongostring := os.Getenv("MONGO_URI")
+	clientOpts := options.Client().ApplyURI(mongostring)
 	client, err := mongo.Connect(context.TODO(), clientOpts)
 	if err != nil {
 		panic(err)
