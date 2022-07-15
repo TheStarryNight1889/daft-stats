73c06ec987a265b41fcb8755799d34f8d50b9b3a
TheStarryNight1889 <molloy.christie@gmail.com>
2022-07-15 20:06:38 +0100

    changed server addr in frontend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 const axios = require('axios').default;
 
-axios.defaults.baseURL = 'http://localhost:3000/query';
+axios.defaults.baseURL = 'http://server:3000/query';
 
 // graph query
 const propertiesQuery = `
